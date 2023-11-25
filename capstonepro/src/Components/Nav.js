c26c448049bac7a996071d2c21556ca68c5e2f87
Muhammad Ali Khan <allusion@Muhammads-MacBook-Pro-2.local>
2023-11-25 04:16:31 -0600

    version05

--- capstonepro/src/Components/Nav.js
+++ capstonepro/src/Components/Nav.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import "./Nav.css"
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 function Nav({highlight}) {
   const style={fontWight:"bold"}
+  const [click,setClick]=useState(false);
+  const [selected,setSelected]=useState("Home")
+
   return (
     <>
-        <ul className='navigation'>
-            <li ><Link to="/">Home</Link></li>
+        <ul className={click?"navigation active2":"navigation"}>
+            <li className={selected==="Home"?"active":"a"} onClick={(e)=>{e.preventDefault();setSelected(()=>"Home")}}><Link to="/">Home</Link></li>
 
-            <li><Link to="/">About</Link></li>
-            <li><Link to="/">Menu</Link></li>
-            <li><Link to="/form"> Reservations</Link></li>
-            <li><Link to="/">Order Online</Link></li>
-            <li><Link to="/">Login</Link></li>
+            <li className={selected==="About"?"active":"a"} onClick={(e)=>{e.preventDefault();setSelected(()=>"About")}}><Link to="/">About</Link></li>
+            <li className={selected==="Menu"?"active":"a"} onClick={()=>setSelected("Menu")}><Link to="/">Menu</Link></li>
+            <li className={selected==="Reservations"?"active":"a"} onClick={()=>setSelected(()=>"Reservations")}><Link to="/form"> Reservations</Link></li>
+            <li className={selected==="Order Online"?"active":"a"} onClick={()=>setSelected("Order Online")}><Link to="/">Order Online</Link></li>
+            <li className={selected==="Login"?"active":"a"} onClick={()=>setSelected("Login")}><Link to="/">Login</Link></li>
         </ul>
+        <div className="mobile" onClick={()=>click?setClick(false):setClick(true)}>
+          <i className={click?"fas fa-times":"fas fa-bars"}/>
+        </div>
     </>
   )
 }
